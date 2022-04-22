ce7ed14533040306a1a2b066e6f8474a0a408e15
Dante Mele Ientile <dantemeleientile@gmial.com>
2022-04-22 14:40:50 -0300

    previo a cambios de estretegia de variables a vectores

--- ej3.c
+++ ej3.c
@@ -6,40 +6,98 @@
 
 } */
 
-/* void resta_vectores (double resta[], double v1[], double v2[], int n){
-    for (int i = 0; i < n; i++){
-        resta[i] = v2[i] - v1[i];
-    }
-} */
 
-/* void resta_vectores (double resta[2], double v1[], double v2[], int n){
+void resta_vectores (double resta[2], double v1[], double v2[], int n){
     for (int i = 0; i < n; i++){
-        resta[i] = v2[i] - v1[i];
+        resta[i] = v1[i] - v2[i];
     }
-} */
+}
 
-double prod_interno (double v1[], double v2[]){
-    double prod_x = v1[0] * v2[0];
-    double prod_y = v1[1] * v2[1];
-    return prod_x + prod_y;
+double prod_interno (double ax, double ay, double bx, double by){
+    return (ax * bx) + (ay * by);
+}
+
+double norma(double ax, double ay, double bx, double by){
+    double resta_x = ax - bx;
+    double resta_y = bx - by;;
+    return sqrt(pow(resta_x, 2) + pow(resta_y, 2));
+}
+
+double computar_escalar(double px, double py, double ax, double ay, double bx, double by){
+    
+    return prod_interno(px-ax, py-ay, bx-ax, by-ay) / pow (norma (bx, by, ax, ay));
+    
+    double resta_1[2];
+    double resta_2[2];
+
+    resta_vectores (resta_1, p, v1, 2);
+    resta_vectores (resta_2, v2, v1, 2);
+    
+
+    return prod_interno(resta_1, resta_2) / pow (norma (v2, v1) ,2);
+
+/* DEBUGIN CON PRINTS */
+    //printf("%f, %f\n", resta_![0], resta[1]);
+    //printf("%f\n", prod_interno(resta_1, resta_2));
+    //printf("%f\n", prod_interno(resta_1, resta_2) / pow (norma (v2, v1) ,2));
+
 }
 
 int main (void){
     //float polilinea[][2] = {{0, 1}, {1, 3}, {1, 5}};
-    //double puntos[][2] = {{1, 1}, {5, 3}};
-    double v1[] = {1, 1};
-    double v2[] = {5, 3};
-    //double resta[2];
-
    // int n = sizeof(polilinea)/sizeof (polilinea[0]); //debe dar 3=3
-    //resta_vectores (resta, v1, v2, 2);
-    //printf("%f, %f\n", resta[0], resta[1]);
-    printf("%f\n", prod_interno(v1, v2));
-}
+    double v1[] = {0, 1};0
+    double v2[] = {1, 3};
 
-double norma (double v1[], double v2[]){
-    double resta_x = v1[0] - v2[0];
-    double resta_y = v1[1] - v2[1];
-    return sqrt(pow(resta_x) + pow(resta_));
+    const float polilinea[][2] = {
+        {0, 1}, 
+        {1, 3}, 
+        {1, 5}, 
+        {2, 5}, 
+        {3, 4}, 
+        {4, 4}, 
+        {5, 3}, 
+        {4, 2}, 
+        {5, 1}, 
+        {2, 0}
+    };
+
+
+
+
+    double p[] = {0, 0};
+    
+    //computar_escalar(p, v2, v1, 2);
+
+/* DEBUGIN CON PRINTS */
+    
+    //printf("%f, %f\n", resta[0], resta[1]);
+    //printf("%f\n", prod_interno(v1, v2));
+    //printf("%f\n", norma(v2, v1));
+    printf("%f\n", computar_escalar(p, v2, v1, 2));
+    
+/* CONDICIONES ALPAH */
+    double resta_3[2];
+    if (computar_escalar(p, v2, v1, 2) <= 0){
+        norma (p, v1);
+        printf("%f\n", norma(p, v1));
+
+    } else if (computar_escalar(p, v2, v1, 2) >= 1){
+        norma (p, v2);
+        printf("%f\n", norma(p, v2));
+
+    } else{
+        resta_vectores (resta_3, p, v1, 2);
+        double vd[2];
+        vd[0] = v1[0] + computar_escalar(p, v2, v1, 2) * resta_3[0];
+        vd[1] = v1[1] + computar_escalar(p, v2, v1, 2) * resta_3[1];
+        
+/* DEBUGIN CON PRINTS dentro del ELSE */
+        //punto d e recta en condición de que el punto esté en el medio
+        printf("%f, %f\n", vd[0], vd[1]);
+
+        //impresión de norma 
+        printf("%f\n", norma(p, vd));
+    }
 }
 
--- test.c
+++ test.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <math.h>
+#define DIMENSION 2
 
-void resta_vectores (double resta[], double v1[][2], int n){
-    for (int i = 0; i < n; i++){
-        resta[i] = v1[i+1][] - v1[i][];
+void resta_vectores (int d, double resta[][d], double **v1, int n){
+    for (int i = 0; i < n-1; i++){
+        for (int j = 0; j < d; j++){
+            resta[i][j] = v1[i+1][j] - v1[i][j];
+            printf("%f, ", resta[i][j]);
+        }
+        printf("\n");
     }
 }
 
-
-
 int main (void){
-    double vectores[][2] = {{1, 1}, {3, 5}};
-    resta_vectores (resta, vectores, n) 
+    double vectores[][DIMENSION] = {{1, 1}, {3, 5}, {2, 2}, {4, 2}};
+    int n = sizeof(vectores) / sizeof(vectores[0]);
+    double resta[n-1][DIMENSION];
+    resta_vectores (resta, (double**)vectores, n, DIMENSION); 
 }
-
-/* (1; 1)
-(3; 5)
-(2; 4) */
