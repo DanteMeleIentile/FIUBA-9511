753f8c7c65165db5689b0f75a4e06df6b2a119f8
Dante Mele Ientile <dantemeleientile@gmial.com>
2022-04-23 15:19:59 -0300

    distancia de punto a polilinea terminado

--- ej3.c
+++ ej3.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-//DIMENTSION -1
 
 #define X 0
 #define Y 1
@@ -15,47 +14,38 @@ double norma(double ax, double ay, double bx, double by){
     return sqrt(pow(resta_x, 2) + pow(resta_y, 2));
 }
 
-double computar_escalar(double px, double py, double ax, double ay, double bx, double by){
+double computar_escalar(float px, float py, double ax, double ay, double bx, double by){
     return prod_interno(px-ax, py-ay, bx-ax, by-ay) / pow (norma (bx, by, ax, ay),2);
 }
 
-double distancia_punto_a_polilinea(const float polilinea[][2], size_t n, double px, double py){
+double distancia_punto_a_polilinea(const float polilinea[][2], size_t n, float px, float py){
     double d_menor = norma (px, py, polilinea[0][X], polilinea[0][Y]);
+    
     double d_temp = 0;
+
     for (int i = 0; i < n-1; i++){
         double ax = polilinea[i][X];
         double ay = polilinea[i][Y];
-
         double bx = polilinea[i+1][X];
         double by = polilinea[i+1][Y];
-
         if (computar_escalar(px, py, ax, ay, bx, by) <= 0){
             d_temp = norma (px, py, ax, ay);
-            
-            //printf("distancia temporal: %f\n\n", d_temp);
-
         } else if (computar_escalar(px, py, ax, ay, bx, by) >= 1){
-            d_temp = norma (py, px, bx, by);
-
-            //printf("distancia temporal: %f\n\n", d_temp);
-
+            d_temp = norma (px, py, bx, by);
         } else{
             double vd[2];
             vd[X] = ax + computar_escalar(px, py, ax, ay, bx, by) * (bx - ax);
             vd[Y] = ay + computar_escalar(px, py, ax, ay, bx, by) * (by - ay);
 
             d_temp = norma (px, py, vd[X], vd[Y]);
-            //printf("distancia temporal: %f\n\n", d_temp);
         }
 
         if (d_menor > d_temp){
             d_menor = d_temp;
         }
     }
-    printf("distancia a polilinea: %f\n\n", d_menor);
-
+    printf("distancia a polilinea: %.8f ", d_menor);
     return d_menor;
-
 }
 
 int main (void){
@@ -72,8 +62,7 @@ int main (void){
         {2, 0}
     };
 
-    //double p[] = {0, 7};
-    double puntos[][2] = {
+    float puntos[][2] = {
         {0, 0},
         {0, 2},
         {6, 1},
@@ -87,24 +76,34 @@ int main (void){
         {0, 7}
     };
 
+    double resultados[] = {
+        1.000000,
+        0.447213,
+        1.000000,
+        1.341640,
+        0.894427,
+        0.000000,
+        0.707106,
+        4.123105,
+        1.000000,
+        0.000000,
+        2.236067
+    };
+
     size_t n = sizeof (polilinea) / sizeof(polilinea[0]);
-    
-    //distancia_punto_a_polilinea (polilinea, n, p[X], p[Y]);
-
     size_t p = sizeof (puntos) / sizeof(puntos[0]);
-
     for (int i = 0; i < p; i++){
-        double px = puntos[i][X];
-        double py = puntos[i][Y];
-        distancia_punto_a_polilinea (polilinea, n, px, py);
-
+        double d_menor = distancia_punto_a_polilinea (polilinea, n, puntos[i][X], puntos[i][Y]);
+        
+        if (resultados[i] != trunc(d_menor*1000000)/1000000){
+            printf ("- RESULTADO INVALIDO\n");
+        } else{
+            printf ("\n");
+        }
     }
-
     return 0;
 }
 
 /* COMENTARIOS */
-// Falta mensaje de error!!!
-//Cambié la función de santisi (tipo de dato!
 // Que onda el cost float
 // compitibilidad con "rotar" y "trasladar"
