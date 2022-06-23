d4deed89818da816acb836d17e2ab18fff3d9c98
Lean <40633153+Leansifon@users.noreply.github.com>
2022-06-23 20:37:39 -0300

    Update text.c
    Bueno no se flaco

--- text.c
+++ text.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 
 int main(){
-<<<<<<< HEAD
-    printf ("hola mundo \n");
-
-    //chau mundo
-
-=======
     printf (" HOLA mundito \n");
     
     //comentario
->>>>>>> b6bcf122ab9c7ee1b5173e2446cf425926e1189f
     return 0;
 
 }
